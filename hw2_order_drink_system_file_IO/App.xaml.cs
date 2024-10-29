520d9aa9b8dc848d0be97db1d5a3f5eab8ab865a
avew0418 <avew.yone@gmail.com>
2024-10-29 20:32:22 +0800

    加入專案檔案。

---
+++ hw2_order_drink_system_file_IO/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace hw2_order_drink_system_file_IO
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
